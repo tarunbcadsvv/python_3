e4b19fb1f22de14a1d2c2a966d303118270e5b3a
tarun <133577577+tarunbcadsvv@users.noreply.github.com>
2024-09-12 14:25:36 +0530

    second commit for folder pattern

---
+++ pattern/02_code.py
@@ -0,0 +1,11 @@
+#    *
+#   **
+#  ***
+# ****
+for i in range(1,5,1):
+    print(' '*(4-i)+'*'*i)
+    #OR
+    # 1). print(' '*(4-i), end=' ')
+    #     print('*'*i)
+    #2). for i in range(1,5,1):
+    #       print(' '*(4-i),'*'*i,sep='')
